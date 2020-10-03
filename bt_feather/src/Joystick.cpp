81e9be2e3dfb9fbfd515f3f2b1d3bed487a7a05d
Chris Jansson <chris@chrisjansson.se>
2020-10-03 20:32:28 +0200

    Define empty placeholders so that all compiles

---
+++ bt_feather/src/Joystick.cpp
@@ -0,0 +1,44 @@
+#include "Joystick.h"
+
+void Joystick_T::button(uint8_t button, bool val)
+{
+}
+
+void Joystick_T::useManualSend(bool useManualSend)
+{
+}
+
+void Joystick_T::setWheel(unsigned int val)
+{
+}
+
+void Joystick_T::X(unsigned int val)
+{
+}
+
+void Joystick_T::Y(unsigned int val)
+{
+}
+
+int Joystick_T::recv(void *buffer, uint16_t timeout)
+{
+  return 0;
+}
+
+void Joystick_T::clutch1(unsigned int val)
+{
+}
+
+void Joystick_T::clutch2(unsigned int val)
+{
+}
+
+void Joystick_T::send_now()
+{
+}
+
+void Joystick_T::hat(int val)
+{
+}
+
+Joystick_T Joystick = Joystick_T();
--- bt_feather/src/Joystick.h
+++ bt_feather/src/Joystick.h
@@ -1,6 +1,8 @@
 #ifndef _JOYSTICK_H_
 #define _JOYSTICK_H_
 
+#include "Arduino.h"
+
 
 class Joystick_T
 {
